3d40fed6d6e932fc557bda1f827eb9f0b661ebc2
aria_m <aria_bs4@yahoo.com>
2020-04-21 00:22:24 +0300

    add jquery an bootstrap to project. build a panel to customise world .add a lot of animation to panel

--- myjs.js
+++ myjs.js
@@ -1,27 +1,27 @@
 window.onload = function() {
     var lifeBox = document.getElementById("lifeBox");
-    var height = 20;
-    var weight = 20;
+    var vertical = 20;
+    var horizental = 20;
     var chanceOfLife = 2;
     var world = new Array();
     var newworld = new Array();
 
     buildWorld();
-    setInterval(function(){ revolution() },800);
+setInterval(function(){ revolution() },800);
     
     // console.log(world.join('\n'));
 
     function buildWorld(){
-        for(var x = 0 ; x < weight ; ++x){
+        for(var x = 0 ; x < horizental ; ++x){
             world[x] = new Array();
             newworld[x] = new Array();
-            for(var y = 0 ; y < height ; ++y){
+            for(var y = 0 ; y < vertical ; ++y){
                 newworld[x][y] = 0;
                 world[x][y] = Math.random()*10 > chanceOfLife ? 0 : 1 ;
                 var box = document.createElement('div');
-                box.id = x.toString() + y.toString() ;
+                box.id = x.toString() +"."+ y.toString() ;
                 box.classList.add("box");
-                //box.innerHTML = world[x][y];
+                // box.innerHTML = (x*vertical) + y;
                 if(world[x][y])
                     box.style.backgroundColor = "#121615";
                 else
@@ -32,12 +32,12 @@ window.onload = function() {
     }
 
     function revolution(){
-        for(var x = 0 ; x < weight ; ++x){
-            for(var y = 0 ; y < height ; ++y){
+        for(var x = 0 ; x < horizental ; ++x){
+            for(var y = 0 ; y < vertical ; ++y){
                 var lives = 0;
                 for(var xd = x-1 ; xd <= x+1 ; ++xd){
                     for(var yd = y-1 ; yd <= y+1 ; ++yd){
-                        if(world[(xd+weight)%weight][(yd+height)%height])
+                        if(world[(xd+horizental)%horizental][(yd+vertical)%vertical])
                             ++lives;
                     }
                 }
@@ -48,8 +48,8 @@ window.onload = function() {
         }
         
 
-        for(var x = 0 ; x < weight ; ++x){
-            for(var y = 0 ; y < height ; ++y){
+        for(var x = 0 ; x < horizental ; ++x){
+            for(var y = 0 ; y < vertical ; ++y){
                 world[x][y] = newworld[x][y];
             }
         }
@@ -59,9 +59,9 @@ window.onload = function() {
 
     function rebuildworld(){
         var box = document.getElementsByClassName("box");
-        for(var x = 0 ; x < weight ; ++x){
-            for(var y = 0 ; y < height ; ++y){
-                var num = (x*weight) + y;
+        for(var x = 0 ; x < horizental ; ++x){
+            for(var y = 0 ; y < vertical ; ++y){
+                var num = (x*vertical) + y;
                 if(world[x][y])
                     box[num].style.backgroundColor = "#121615";
                 else
---
+++ setting.js
@@ -0,0 +1,198 @@
+$(document).ready(function () {
+    var h = $('#settings').offset().top;
+    $('#forsetting').css("top", eval(h - 7));
+    var isvisible = false;
+    var isfinish = true;
+
+    var isvisibleabout = false;
+    var isfinishabout = true;
+
+    var sit = new Array(2);
+    var return_element,returnseting;
+
+    var store ;
+    var triger;
+    var answer = new Object();
+    
+    $('#settings').on( "click", function() {
+        if($(".areapaper").hasClass("onboard") &&  ($(".onboard").attr('id') != 'settingChange' )){
+            console.log("call if in seting");
+            console.log("this is whocalld"+$(".onboard").attr("whocalld"));
+            repo(true,$(".onboard").attr("whocalld"),"settings");
+            $("#"+$(".onboard").attr("whocalld")).trigger( "click" );
+        }else{
+            console.log("call else in setting");
+            clickonset();
+        }
+        
+        
+        
+
+    });
+
+    $('#About').on( "click", function() {
+        if($(".areapaper").hasClass("onboard") && ($(".onboard").attr('id') != 'aboutArea' )){
+            console.log("call if in about");
+            repo(true,$(".onboard").attr("whocalld"),"About");
+            $("#"+$(".onboard").attr("whocalld")).trigger( "click" );
+        }else{
+            console.log("call else in about");
+            clickonabout();
+        }
+        
+    });
+
+    function clickonset(){
+        returnseting = close_setting();
+        open_seting();
+        return returnseting;
+    };
+
+    function clickonabout(){
+        if($('#aboutArea').is(":visible") && isvisibleabout && isfinishabout){
+            //close about paper
+            isfinishabout = false;
+            $("#About span").hide(400);
+            $(".onboard").animate({top: "420px"},400,function(){
+                $(".onboard").removeClass("onboard").hide();
+                console.log('Animation ended');
+                answer["0"] = answer["1"] = 0;
+                console.log(answer);
+                answer = repo(false);
+                console.log(answer);
+                if((answer["0"] == "About")){
+                    console.log("in the repo of about");
+                    $("#"+answer[1]).trigger( "click" );
+                }
+                isfinishabout = true;
+                isvisibleabout = false;
+            });
+            //return $("#aboutArea");
+        }
+        if(!isvisibleabout && !$('#aboutArea').is(":visible")){
+            //open about paper
+            isvisibleabout = true; 
+            $("#About span").addClass("clicked").show(400);
+            // close_all_paper();
+            $("#aboutArea").show().addClass("onboard").animate({top: "0px"},400);
+        }
+    }
+
+    function close_setting(){
+        if ($('#selectArea').is(":visible") && isvisible && isfinish) {
+            //close the setting
+            isfinish=false;
+            $('#selectArea').stop(true, false);
+            $('.settingItem').stop(true, false);
+            $('#settingArea').stop(true, false);
+            var items = $('.settingItem').length;
+            //close the paper of setting
+            $(".onboard").animate({top: "420px"},300,function(){
+                $(".onboard").removeClass("onboard").hide()
+            });
+            //
+            $("#settings span").hide(1000);
+            $('.settingItem').each(function () {
+                $(this).animate({ opacity: "0", top: "150px" }, 500 + (items-- * 100), function () {
+                    if (items == 0) {
+
+                        $('#selectArea').animate({ width: "0" }, 500, function () {
+                            $('#selectArea').hide();
+                        });
+                        $('#settingArea').animate({ width: "82%" }, 500, function () {
+                            isfinish = true;
+                            isvisible = false;
+                            $("#settings span").removeClass("clicked");
+                            $(".flash").animate({top:  "0px"},500);
+                            console.log('Animation ended');
+                            answer["0"] = answer["1"] = 0;
+                            console.log(answer);
+                            answer = repo(false);
+                            console.log(answer);
+                            if((answer["0"] == "settings")){
+                                console.log("in the repo of setting");
+                                $("#"+answer[1]).trigger( "click" );
+                            }
+                        });
+                        return $('#settingArea');
+                    }
+                });
+            });
+        }
+    };
+
+    function open_seting(){
+        if(!isvisible && !($('#selectArea').is(":visible"))) {
+            //open the setting
+            isvisible =true;
+            $('#selectArea').stop(true, false);
+            $('.settingItem').stop(true, false);
+            $('#settingArea').stop(true, false);
+            $("#settings span").addClass("clicked").show(1000);
+            $("#settingChange").addClass("onboard")
+
+            $('#settingArea').animate({ width: "75%" }, 400);
+            $('#selectArea').animate({ width: "7%" }, 400, function () {
+                //should put all papers down here   ******************************done
+                //sit = close_all_paper();
+                $("#settingChange").show().animate({top: "0px"},400);
+                //show the items in middle area
+                var items = $('.settingItem').length;
+                $('.settingItem').each(function () {
+                    $(this).animate({ opacity: "1", top: "0px" }, 300 + (items++ * 100));
+                });
+            });
+            $('#selectArea').show();   
+        }
+    };
+
+    function repo(state ,request = "-1" , checkme = "-1"){
+        if(!state){
+            var ans = new Object();
+            ans = {"0": store , "1": triger};
+            store = triger = 0;
+            console.log("asn = " + ans);
+            console.log(ans);
+            return ans;
+        }else {
+            store = request;
+            triger = checkme;
+            // console.log(store +" " + triger);
+            return {"0" : 0 , "1" : 0};
+        }
+    }
+
+   
+    $(".newcolor").hover(
+        function(){ $(this).css("background-color", "var(--color2-hover)") },
+        function(){ $(this).css("background-color", "var(--color2)") });
+
+
+    //************************************** */
+    //************************************** */
+    //    world setting
+
+    $('.inputs').keydown(function(event){
+        console.log(event.keyCode);    // it works
+       // console.log($(this).val());    // it works
+       //if( isFinite( $(this).val() )){
+       var keycode = parseInt(event.keyCode);
+       if( (keycode <= 105) && (keycode >= 96)  ){
+           //here chould put number in what i want
+           if( !($(this).val()) && (keycode == 96) ){
+               console.log("zero");
+               //span should be you cant put zero size
+               return false;
+           }
+           
+       }else if(keycode == 8){
+           
+       }else{
+           // clinet input character
+          
+           console.log("character");
+           return false;
+       }
+       // // $(this).
+   });
+});
